8c48596f047f9b37042a3f4f0bdf68a976ca108a
Isaamarod <belamarod@gmail.com>
2019-04-29 13:21:21 +0200

    word count

--- src/main/scala/wordCount.scala
+++ src/main/scala/wordCount.scala
@@ -1,6 +1,15 @@
 import org.apache.spark.{SparkContext, SparkConf}
-object wordCount extends App {
+object wordCount {
 
-    println("Hello, World!")
+    def main(args: Array[String]): Unit = {
+        val conf = new SparkConf().setAppName("Simple Application").setMaster("local")
+          val sc = new SparkContext(conf)
+          val data = sc.textFile("/home/isa/Escritorio/spark-2.4.1-bin-hadoop2.7/README.md").cache()
+          val wcSpark = data.flatMap(l => l.split(" ")).filter(x => x == "Spark").map(word =>
+          (word,1)).reduceByKey(_ + _)
+          wcSpark.foreach(println)
+          val wc = data.flatMap(_.split(" ")).map((_,1)).reduceByKey(_ + _)
+          wc.foreach(println)
 
-}
+          }
+          }
