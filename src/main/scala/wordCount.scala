21203d15421d007a53d9803d3d0e32e93c167ad7
Isaamarod <belamarod@gmail.com>
2019-05-10 20:05:44 +0200

    folders changes@

--- src/main/scala/wordCount.scala
+++ src/main/scala/wordCount.scala
@@ -4,12 +4,13 @@ object wordCount {
     def main(args: Array[String]): Unit = {
         val conf = new SparkConf().setAppName("Simple Application").setMaster("local")
           val sc = new SparkContext(conf)
-          val data = sc.textFile("/home/isa/Escritorio/spark-2.4.1-bin-hadoop2.7/README.md").cache()
-          val wcSpark = data.flatMap(l => l.split(" ")).filter(x => x == "Spark").map(word =>
-          (word,1)).reduceByKey(_ + _)
+          val data = sc.textFile("./src/main/resources/shakespeare.txt").cache()
+          val wcSpark = data.flatMap(l => l.split(" ")).map(word =>(word,1)).reduceByKey(_ + _)
           wcSpark.foreach(println)
           val wc = data.flatMap(_.split(" ")).map((_,1)).reduceByKey(_ + _)
           wc.foreach(println)
+          wc.saveAsTextFile(path= "./src/main/tmp/shakespeareWordCountOutput_3")
 
-          }
+
+    }
           }
